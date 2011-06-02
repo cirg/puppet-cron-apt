5debdffd0412653b06c1dd9a0c3f99ce6b522dda
Svend Sorensen <svends@uw.edu>
2011-06-02 13:46:48 -0700

    Install default cron-apt config file

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,9 @@ class cron-apt {
   package { 'cron-apt':
     ensure => installed,
   }
+
+  file { '/etc/cron-apt/config':
+    content => template('cron-apt/config.erb'),
+    require => Package['cron-apt'],
+  }
 }
