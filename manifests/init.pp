65bdc3b930612453d8880e9ef669a21464978b8f
Svend Sorensen <svends@uw.edu>
2011-06-02 14:20:27 -0700

    Add optional upgrade action

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
 class cron-apt (
-  $mailon = 'error'
+  $mailon = 'error',
+  $upgrade = false
 ) {
   package { 'cron-apt':
     ensure => installed,
@@ -7,6 +8,18 @@ class cron-apt (
 
   file { '/etc/cron-apt/config':
     content => template('cron-apt/config.erb'),
+    require => Package['cron-apt'],
+  }
+
+  if $upgrade {
+    $upgrade_ensure = present
+  } else {
+    $upgrade_ensure = absent
+  }
+
+  file { '/etc/cron-apt/action.d/5-upgrade':
+    ensure  => $upgrade_ensure,
+    source  => 'puppet:///cron-apt/action.upgrade',
     require => Package['cron-apt'],
   }
 }
